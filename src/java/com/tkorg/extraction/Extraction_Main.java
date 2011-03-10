ad51b7822c31fb1032b900ff97288bcc73607399
trandanh882006 <trandanh882006@gmail.com>
2011-03-10 15:10:07 +0000

    hoan chinh rut trich

--- src/java/com/tkorg/extraction/Extraction_Main.java
+++ src/java/com/tkorg/extraction/Extraction_Main.java
@@ -22,10 +22,12 @@ import java.util.logging.Logger;
  */
 public class Extraction_Main {
 
-    ArrayList < com.tkorg.extraction.MyFile > fileList = null;
+    public ArrayList < com.tkorg.extraction.MyFile > fileList = null;
+    public ArrayList < MyKeyword > keywordList = null;
 
     public Extraction_Main() {
         fileList = new ArrayList < com.tkorg.extraction.MyFile >();
+        keywordList = new ArrayList < MyKeyword >();
     }
 
     public void loadFiles() {
@@ -41,7 +43,8 @@ public class Extraction_Main {
                     String text = "";
                     String content = "";
                     while ((text = reader.readLine()) != null) {
-                        content = content + text + "\n";
+                        if (!text.equals(""))
+                            content = content + text + "\n";
                     }
                     ArrayList < String > temp = new ArrayList < String >();
                     for (int j = 0, index = 0; j < content.length() - 1; j++) {
@@ -70,6 +73,52 @@ public class Extraction_Main {
         }
     }
 
+    public String extractSentence(String sentence, String keywordname) {
+        String individual = "";
+        for (int j = 0; j < Constants.removewords.length; j++) {
+            if (sentence.toLowerCase().startsWith(Constants.removewords[j].toLowerCase())) {
+                sentence = sentence.substring(Constants.removewords[j].length()).toString();
+                if (sentence.startsWith(",")) {
+                    sentence = sentence.substring(2);
+                }
+            }
+        }
+
+        if (sentence.toLowerCase().startsWith(keywordname) || sentence.toLowerCase().startsWith(" " + keywordname)) {
+            for (int j = 0; j < Constants.followwords.length; j++) {
+                if (sentence.matches(keywordname + Constants.followwords[j] + ".*") || sentence.matches(keywordname + " \\(.*\\)" + Constants.followwords[j] + ".*")) {
+                    individual = sentence.substring(keywordname.length() + Constants.followwords[j].length() + 1);
+                    break;
+                }
+            }
+        } else if (sentence.endsWith(keywordname)) {
+            for (int j = 0; j < Constants.forwardwords.length; j++) {
+                if (sentence.endsWith(Constants.forwardwords[j] + keywordname)) {
+                    individual = sentence.substring(0, sentence.length() - (Constants.forwardwords[j].length() + keywordname.length()));
+                    break;
+                }
+            }
+        }
+
+        return individual;
+    }
+
+    public void extractWithKeywords(ArrayList < String > keywordnames) {
+        for (int index = 0; index < keywordnames.size(); index++) {
+            MyKeyword keyword = new MyKeyword();
+            keyword.setName(keywordnames.get(index));
+            for (int i = 0; i < fileList.size(); i++) {
+                for (int j = 0; j < fileList.get(i).getSentences().size(); j++) {
+                    String temp = extractSentence(fileList.get(i).getSentences().get(j), keyword.getName());
+                    if (!temp.equals("")) {
+                        keyword.getIndividuals().add(temp);
+                    }
+                }
+            }
+            keywordList.add(keyword);
+        }
+    }
+
     public ArrayList<MyFile> getFileList() {
         return fileList;
     }
@@ -78,14 +127,33 @@ public class Extraction_Main {
         this.fileList = fileList;
     }
 
+    public ArrayList<MyKeyword> getKeywordList() {
+        return keywordList;
+    }
+
+    public void setKeywordList(ArrayList<MyKeyword> keywordList) {
+        this.keywordList = keywordList;
+    }
+
     public static void main(String[] args) {
         Extraction_Main m = new Extraction_Main();
 
         m.loadFiles();
-        for (int i = 0; i < m.getFileList().size(); i++) {
-            for (int j = 0; j < m.getFileList().get(i).getSentences().size(); j++) {
-                System.out.println(m.getFileList().get(i).getSentences().get(j));
+        ArrayList < String > keywords = new ArrayList < String >();
+        keywords.add("tin học");
+        keywords.add("hệ điều hành mạng");
+        m.extractWithKeywords(keywords);
+        for (int i = 0; i < m.getKeywordList().size(); i++) {
+            System.out.println(m.getKeywordList().get(i).getName());
+            for (int j = 0; j < m.getKeywordList().get(i).getIndividuals().size(); j++) {
+                System.out.println("    " + m.getKeywordList().get(i).getIndividuals().get(j));
             }
         }
+
+//        for (int i = 0; i < m.getFileList().size(); i++) {
+//            for (int j = 0; j < m.getFileList().get(i).getSentences().size(); j++) {
+//                System.out.println(m.getFileList().get(i).getSentences().get(j));
+//            }
+//        }
     }
 }
