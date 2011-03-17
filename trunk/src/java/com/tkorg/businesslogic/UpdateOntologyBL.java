e2266b1842c7d444aebe8f73e1bb53910e1724b9
trandanh882006@gmail.com <trandanh882006@gmail.com@160e4f81-d1e1-36c3-ca67-b3a65d553466>
2011-03-17 08:49:08 +0000

    them ITDocumentBL.java va UpdateOntologyBL.java

---
+++ trunk/src/java/com/tkorg/businesslogic/UpdateOntologyBL.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tkorg.businesslogic;
+
+import com.tkorg.entities.OWLModel;
+import com.tkorg.extraction.Extraction_Main;
+import com.tkorg.extraction.MyKeyword;
+import com.tkorg.util.Constants;
+import edu.stanford.smi.protegex.owl.model.OWLDatatypeProperty;
+import edu.stanford.smi.protegex.owl.model.OWLIndividual;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
+
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author DANHIT
+ */
+public class UpdateOntologyBL {
+
+    public ArrayList < MyKeyword > selectedItemsList = null;
+
+    public UpdateOntologyBL() {
+        selectedItemsList = new ArrayList < MyKeyword >();
+    }
+
+    public void choseItems(String[] item) {
+        for (int i = 0; i < item.length; i++) {
+            String[] temp = item[i].split("-");
+            int temp01 = Integer.parseInt(temp[0]);
+            int temp02 = Integer.parseInt(temp[1]);
+            Extraction_Main.keywordList.get(temp01).getIndividuals().remove(temp02);
+        }
+        for (int i = 0; i < Extraction_Main.keywordList.size(); i++) {
+            selectedItemsList.add(Extraction_Main.keywordList.get(i));
+        }
+    }
+
+    public void inputOntology() {
+        try {
+            for (int i = 0; i < selectedItemsList.size(); i++) {
+                OWLNamedClass keyword = OWLModel.owlModel.getOWLNamedClass(selectedItemsList.get(i).getName());
+                OWLDatatypeProperty property = OWLModel.owlModel.getOWLDatatypeProperty("Định_nghĩa");
+                for (int j = 0; j < selectedItemsList.get(i).getIndividuals().size(); j++) {
+                    OWLIndividual individual = keyword.createOWLIndividual(keyword.getBrowserText() + "_" + j);
+                    property.setDomain(keyword);
+                    individual.setPropertyValue(property, selectedItemsList.get(i).getIndividuals().get(j));
+                }
+            }
+            URI uri = new File(Constants.PATH_ONTOLOGY).toURI();
+            OWLModel.owlModel.save(uri);
+        } catch (Exception ex) {
+            Logger.getLogger(UpdateOntologyBL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
