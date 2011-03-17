79a47719e4d9e317b7760e1d45d048d741432803
trandanh882006 <trandanh882006@gmail.com>
2011-03-17 08:46:59 +0000

    them ITDocumentAction.java va UpdateOntologyAction.java

---
+++ src/java/com/tkorg/actions/UpdateOntologyAction.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tkorg.actions;
+
+import com.tkorg.businesslogic.UpdateOntologyBL;
+import com.tkorg.forms.UpdateOntologyForm;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ *
+ * @author DANHIT
+ */
+public class UpdateOntologyAction extends org.apache.struts.action.Action {
+    
+    /* forward name="success" path="" */
+    private static final String SUCCESS = "success";
+    
+    /**
+     * This is the action called from the Struts framework.
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     * @throws java.lang.Exception
+     * @return
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+
+        UpdateOntologyForm updateOntologyForm = (UpdateOntologyForm) form;
+
+        String screenid = updateOntologyForm.getScreenid();
+        String processid = updateOntologyForm.getProcessid();
+        String[] item = updateOntologyForm.getItem();
+
+        if (screenid.equals("UPDATE_ONTOLOGY")) {
+            if (processid.equals("UPDATE_ONTOLOGY_01")) {
+                UpdateOntologyBL displayBL = new UpdateOntologyBL();
+                displayBL.choseItems(item);
+                displayBL.inputOntology();
+
+                return mapping.findForward(SUCCESS);
+            }
+        }
+
+        return mapping.findForward(SUCCESS);
+    }
+}
