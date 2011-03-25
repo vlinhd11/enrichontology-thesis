fb905a4e7700c2329897347724d14a0ed9c1bd01
trandanh882006@gmail.com <trandanh882006@gmail.com@160e4f81-d1e1-36c3-ca67-b3a65d553466>
2011-03-25 08:44:40 +0000

    ResultForm.java

---
+++ trunk/src/java/com/tkorg/forms/ResultForm.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tkorg.forms;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ *
+ * @author DANHIT
+ */
+public class ResultForm extends org.apache.struts.action.ActionForm {
+
+    private String screenid;
+    private String processid;
+
+    public String getProcessid() {
+        return processid;
+    }
+
+    public void setProcessid(String processid) {
+        this.processid = processid;
+    }
+
+    public String getScreenid() {
+        return screenid;
+    }
+
+    public void setScreenid(String screenid) {
+        this.screenid = screenid;
+    }
+
+    /**
+     *
+     */
+    public ResultForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * This is the action called from the Struts framework.
+     * @param mapping The ActionMapping used to select this instance.
+     * @param request The HTTP Request we are processing.
+     * @return
+     */
+    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+//        if (getName() == null || getName().length() < 1) {
+//            errors.add("name", new ActionMessage("error.name.required"));
+//            // TODO: add 'error.name.required' key to your resources
+//        }
+        return errors;
+    }
+}
